8936863e3f37ddb7f8550e6ad7ddf8f1571fad5b
Crypto-V <58492579+Crypto-V@users.noreply.github.com>
2022-09-28 11:54:18 +0100

    Validation and Linear Algorithms

---
+++ src/Algorithms/ValidationAlgorithms.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+public class ValidationAlgorithms {
+//    With this algorithm you can validate if the given input has certain parameters or attributes.
+//   Example:  Check if string have all upper or lower case letter.
+//    Normally the output is boolean
+
+        // This algorithm will check if all letter are uppercase.?
+    public static boolean isUppercase(String s) {
+        // returns true if all uppercase and false if not.
+        return s.chars().allMatch(Character::isUpperCase);
+    }
+
+    public static boolean isLowercase(String s){
+        // Another way is to use noneMatch:
+        // s.chars().noneMatch(Character::isUpperCase);
+        return s.chars().allMatch(Character::isLowerCase);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isUppercase("hello"));
+        System.out.println(isUppercase("HELLO"));
+
+        System.out.println(isLowercase("Hello"));
+        System.out.println(isLowercase("heLLo"));
+
+
+
+
+    }
+
+}
+
